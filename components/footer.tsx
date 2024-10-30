e42b91e9f700bcec5bee5fcb938a5ec6b6f74554
Andres Hung <andres.hung@outlook.com>
2024-10-30 15:33:21 -0600

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,12 +7,13 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="footer">
-      <span>
-        CodeCraft Solutions &copy; 2024 |{" "}
-        <Link href="https://github.com/andreshungbz/ub-treetrack">
-          GitHub Repository
-        </Link>
-      </span>
+      <Link
+        href="https://github.com/andreshungbz/ub-treetrack"
+        className="text-blue-900 underline"
+      >
+        GitHub Repository
+      </Link>
+      <span>CodeCraft Solutions &copy; 2024</span>
     </footer>
   );
 }
